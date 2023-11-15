286965f2b226b9405f3e262553d6b8610f0b4789
mbeal_sgp6gtp <mbeal_sgp6gtp@100.66.154.76>
2023-11-15 14:30:56 -0500

    making InsertSL

---
+++ Lab8/InsertSL.java
@@ -0,0 +1,25 @@
+package Lab8;
+
+public class InsertSL<T> extends SkipList{
+	
+	@Override
+	public void insert(Comparable value) {
+		
+		
+	}
+
+
+	@Override
+	public boolean contains(Comparable value) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+	@Override
+	public boolean delete(Comparable value) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+}
